3eb82ae60fde3360e129014d982b07310bc60ca9
Manologk <46338492+Manologk@users.noreply.github.com>
2021-11-22 11:10:40 +0300

    now JS is working

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-const X_CLASS = 'x';
-const CIRCLE_CLASS ='o';
-const cellElements = document.querySelectorAll('[data-cell]');
+const X_CLASS = "x";
+const CIRCLE_CLASS = "o";
+const cellElements = document.querySelectorAll("[data-cell]");
 let circleTurn;
 
+cellElements.forEach((cell) => {
+  cell.addEventListener("click", handleClick, { once: true });
+});
 
-cellElements.forEach(cell =>{
-    cell.addEventListener('click', handleClick, { once: true})
-  })
-
-
-function handleClick(e){
-    //place the mark
-    //check for draw
-    // check for win
-    //switch turns
-    console.log("clicked")
-
-//     const cell =  e.target;
-//     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-
-//     placeMark(cell, currentClass);
-//   }
-
-// function placeMark(cell, currentClass){
-//     cell.classList.add(currentClass);
+function handleClick(e) {
+  console.log("clicked");
 }
-
